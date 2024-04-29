86fbd2a8effefbd3d9a2dedb249a31645b288deb
zeyqam <zegamda@code.edu.az>
2024-04-30 02:25:22 +0400

    end task

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,5 +98,70 @@ let nums = [1, 4, 5, 6, 9];
 // console.log(name.customContains("c"));
 // console.log(surname.customContains("A"));
 
-let date = new Date();
-console.log(date);
+// let date = new Date();
+// //console.log(date);
+// let year = date.getFullYear();
+// // console.log(year);
+// let month = date.getMonth();
+
+// let result = `${month + 1}-${year}`;
+// console.log(result);
+
+// let data = Math.sqrt(64);
+// console.log(data);
+
+//DOM
+
+// let elems = document.getElementsByTagName("h1");
+
+// for (let i = 0; i < elems.length; i++) {
+//   const element = elems[i];
+//   console.log(elems);
+// }
+
+// let elem = document.getElementsByClassName("test");
+// console.log(elem);
+
+//important
+// let elem = document.getElementById("products");
+// console.log(elem);
+
+// let elems = document.querySelectorAll("#products h1");
+// elems[1].style.backgroundColor = "Red";
+
+// for (let i = 0; i < elems.length; i++) {
+//   const element = elems[i];
+//   console.log(element);
+// }
+// for (const item of elems) {
+//   item.style.backgroundColor = "Red";
+// }
+
+//let h1 = document.querySelector("h1");
+//console.log(h1.innerText);
+//console.log(h1.innerHTML);
+//h1.innerText = "Reshad Bey";
+// h1.style.color = "Teal";
+// h1.style.margin="200px"
+//console.log(document.querySelector("#products ul li:nth-child(2)"));
+//h1.className="active"
+// h1.classList.add("active");
+// h1.classList.remove("product");
+//console.log(h1.classList.contains("active"));
+// h1.setAttribute("id", "text");
+// console.log(h1.getAttribute("id"));
+// console.log(h1.hasAttribute("id"));
+
+let button = document.querySelector(".add-product");
+let h1 = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+  this.style.backgroundColor = "Magenta";
+  h1.style.backgroundColor = "Green";
+  h1.style.color = "White";
+  h1.style.width = "200px";
+  h1.innerText = "salam fatime xanim";
+  let btnText = this.innerText;
+  this.innerText = h1.innerText;
+  h1.innerText = btnText;
+});
